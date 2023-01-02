bf97a6197466b07214d32bc8ae478012ad9dea85
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-02 13:27:09 +0100

    feat: use global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import {resolve} from "path";
 dotenv.config({path: resolve(__dirname, "../.env")});
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validateCustomDecorators: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
