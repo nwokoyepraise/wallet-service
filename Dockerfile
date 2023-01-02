ff1ba768bf8de21d61e0102e9f1f920bef550135
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-02 11:35:15 +0100

    chore: init docker and db dependencies

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:18
+
+#WORKDIR /wallet-service
+COPY package.json .
+RUN npm install
+COPY . .
+CMD npm run build && npm run start
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { KnexModule } from 'nestjs-knex';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    KnexModule.forRoot({
+      config: {
+        client: 'mysql2',
+        connection: {
+          host: 'localhost',
+          user: process.env.MYSQLDB_USER,
+          password: process.env.MYSQLDB_ROOT_PASSWORD,
+          database: process.env.MYSQLDB_DATABASE,
+          port: Number(process.env.MYSQLDB_DOCKER_PORT)
+        },
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
