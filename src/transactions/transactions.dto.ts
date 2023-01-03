a7750eccf562070a131142f5245befa830d42179
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-03 15:19:34 +0100

    feat: use validators

--- src/transactions/transactions.dto.ts
+++ src/transactions/transactions.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from "class-validator";
 import { Iso4217 } from "src/common/enums";
 
 export class FundWalletDto {
+    @IsNotEmpty()
+    @IsNumber()
     amount: number;
+
+    @IsOptional()
+    @IsEnum(Iso4217, {message: 'currency must be a valid Iso 4217 value'})
     currency?: Iso4217 =  Iso4217.NGN
 }
