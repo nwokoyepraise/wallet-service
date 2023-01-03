2417567157d0719eba56a46e5a118269f929fb13
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-03 14:21:17 +0100

    fix: import passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule, JwtModule.register({
+  imports: [PassportModule, UsersModule, JwtModule.register({
     secret: process.env.JWT_SECRET,
     signOptions: { expiresIn: '7d' },
   }),],
