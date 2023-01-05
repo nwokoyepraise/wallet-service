1b7a56f0031b5058143f60026573fb43283226d7
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-05 20:02:50 +0100

    chore: add production settings

--- knexfile.ts
+++ knexfile.ts
@@ -15,4 +15,18 @@ module.exports = {
       directory: './src/db/migrations',
     },
   },
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.MYSQLDB_USER,
+      password: process.env.MYSQLDB_ROOT_PASSWORD,
+      database: process.env.MYSQLDB_DATABASE,
+      port: Number(process.env.MYSQLDB_PORT),
+    },
+    migrations: {
+      extension: 'ts',
+      directory: './src/db/migrations',
+    },
+  },
 };
