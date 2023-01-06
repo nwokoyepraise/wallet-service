43be415b0b3700feb8fad4fe99262c64a6047396
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-06 09:27:43 +0100

    test: add add-user negative test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { VerifiedEmailAlreadyExistsException } from '../common/exceptions';
+import { EmailAlreadyUsedException, VerifiedEmailAlreadyExistsException } from '../common/exceptions';
 import { UsersController } from './users.controller';
 import { AddUserDto, User } from './users.dto';
 import { UsersService } from './users.service';
@@ -69,6 +69,31 @@ describe('UsersController', () => {
       });
 
       expect(async ()=> {await usersController.addUser(addUserDto)}).rejects.toThrowError(VerifiedEmailAlreadyExistsException())
+    });
+
+    it('should not add user because email exists but not verified', async () => {
+      const addUserDto: AddUserDto = {
+        user_id: 'USDJIJE99HJJO',
+        email: 'email@email.com',
+        password: 'hashed-password',
+      };
+
+      jest.spyOn(usersService, 'addUser').mockImplementation((addUserDto) => {
+        return Promise.resolve(fakeUser);
+      });
+
+      jest.spyOn(usersService, 'findUser').mockImplementation(() => {
+        return Promise.resolve({
+          user_id: 'USDJIJE99HJJO',
+          email: 'email@email.com',
+          password: 'hashed-password',
+          email_verified: 0,
+          created_at: date,
+          updated_at: date,
+        });
+      });
+
+      expect(async ()=> {await usersController.addUser(addUserDto)}).rejects.toThrowError(EmailAlreadyUsedException())
     });
 
     it('should add a user', async () => {
