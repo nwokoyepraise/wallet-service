a3d38c3e200935bbbc32d7559fedecaae557f742
nwokoyepraise <nwokoyepraise@gmail.com>
2023-01-27 17:57:23 +0100

    fix: use fetch api

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-const http = require('http');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 //keep awake
 const keepAwake = () => {
-  setInterval(function () {
-    http.get('https://nwokoyepraise-lendsqr-be-test.onrender.com');
+  setInterval(async function () {
+    await fetch('https://nwokoyepraise-lendsqr-be-test.onrender.com');
   }, 300000); // every 5 minutes (300000)
 };
 
